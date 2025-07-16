d319541d01f69eae07dba3b403bdeab1afd9d5ff
prov332325 <prov332325@gmail.com>
2025-07-16 22:24:43 +0900

    불필요한 컴포넌트 제거 및 App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import ThreeDPortfolio from "./components/ThreeDPortfolio";
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
       <Header />
       <Hero />
       <About />
-      <ThreeDPortfolio />
-      <Projects />
       <Contact />
     </>
   );
